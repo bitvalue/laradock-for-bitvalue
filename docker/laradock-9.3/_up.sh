51247e5b4474aa4f140c44266e9840a742a7aa45
Mitsutoshi Nakamura <mitsutoshi.nakamura.jp@gmail.com>
2019-12-24 18:31:20 +0900

    [ change ] laradock-9.3

---
+++ docker/laradock-9.3/_up.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Change containers to fit your needs
+# So you don't have to remember
+docker-compose up -d apache2
+# docker-compose up -d --build apache2
---
+++ docker/laradock-9.3/_workspace.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Login workspace container as laradock user
+docker exec -it -u laradock -e TERM=$TERM $(docker ps -q -f name=work) bash
+
+# Change $COLUMNS $LINES
+# echo 'stty rows 67 cols 227' >> ~/.bashrc
